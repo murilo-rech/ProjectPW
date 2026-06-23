dc69f81eb1e523a6d119c6038c6c17613fbbc964
Murilo Fazenda Rech <murilofazendarech777@gmail.com>
2026-06-22 23:40:55 -0300

    Add files via upload

---
+++ api/source/Config/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+const CONF_URL_BASE = "http://localhost/acme-3at"; // URL base do site, geralmente localhost em desenvolvimento
+const CONF_URL_TEST = "http://localhost/acme-3at"; // URL base do site, geralmente localhost em desenvolvimento
+
+const CONF_DB_HOST = "mysql";
+const CONF_DB_NAME = "db-acme-tarde";
+const CONF_DB_USER = "root";
+const CONF_DB_PORT = "3306";
+const CONF_DB_PASS = "1234567";
+
+const JWT_SECRET_KEY = "e17851db9fee8e49f728550fc2f82111c4374f426c9cadda9403390ef638073ff21fd7a7e35d025861e175b0fb93609d838fba3185c859c13b9f43cf92ecbd48";
